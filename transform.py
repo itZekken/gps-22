58999ab65ec3de93306bd8fcf84fd19a2b6d8755
Carles Aguilera <carlesaguilera@MacBook-Air-de-Carles.local>
2024-12-20 10:46:32 +0100

    EL FITXER .YML

--- transform.py
+++ transform.py
@@ -5,11 +5,9 @@ def to_upper_case(string):
     """string to upper case"""
     return string.upper()
 
-
 def to_lower_case(string):
     """ string to lower case"""
     return string.lower()
-
 
 def to_capitalize(string):
     """ string capitalized"""
