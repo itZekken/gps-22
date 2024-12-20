84bf2702d63b2da291beab40d03f53c0896ef222
Alex Cantarero Fuentes <87870624+itZekken@users.noreply.github.com>
2024-12-20 11:25:16 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,25 +2,23 @@ import unittest
 import transform
 
 
-"""This module contains test cases for string transformation functions
-from the transform module, such as to_upper_case, to_lower_case, and 
-to_capitalize. These functions are tested to ensure they behave as expected."""
+"""Tests"""
 
 class TestStringMethods(unittest.TestCase):
-    """Test cases for string transformation functions in the transform module."""
+    """Test transformacio de strings."""
 
     def test_is_upper(self):
-        """Test if the to_upper_case function correctly converts a string to uppercase."""
+        """Testeja si es majuscula."""
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
-        """Test if the to_lower_case function correctly converts a string to lowercase."""
+        """Testeja si es minuscula."""
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
-        """Test if the to_capitalize function correctly capitalizes a string."""
+        """Testeja si ho posa en majuscula."""
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
